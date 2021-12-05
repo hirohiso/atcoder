a41ed5290bcef3ce88ddd8dcee11f32485ac274c
hiro <irie.h126@gmail.com>
2021-12-05 16:30:42 +0900

    abc206 a

---
+++ src/main/java/old/abc206/a/Main.java
@@ -0,0 +1,129 @@
+package old.abc206.a;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.NoSuchElementException;
+
+public class Main {
+
+    // https://atcoder.jp/contests/abc206/tasks/abc206_a
+    public static void main(String[] args) {
+        solve(System.in, System.out);
+    }
+
+    public static void solve(InputStream in, PrintStream out) {
+        PrintWriter pw = new PrintWriter(out);
+        FastScanner fs = new FastScanner(in);
+        //==================
+        int n = fs.nextInt();
+
+        //==================
+
+        int price = (int)Math.floor(1.08d * (double) n);
+        if (price == 206){
+            pw.println("so-so");
+        }else if (price > 206){
+            pw.println(":(");
+        }else{
+            pw.println("Yay!");
+        }
+        pw.flush();
+    }
+
+    //-------------------------------------------------------------------
+    public static class FastScanner {
+        InputStream in;
+        byte[] buffer = new byte[1 << 10];
+        int length = 0;
+        int ptr = 0;
+
+        public FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private boolean hasNextByte() {
+            if (ptr < length) {
+                return true;
+            }
+            try {
+                length = in.read(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ptr = 0;
+            return length != 0;
+        }
+
+
+        private byte read() {
+            if (hasNextByte()) {
+                return buffer[ptr++];
+            }
+            return 0;
+        }
+
+        private void skip() {
+            while (hasNextByte() && !isPrintable(buffer[ptr])) {
+                ptr++;
+            }
+        }
+
+        private boolean hasNext() {
+            skip();
+            return hasNextByte();
+        }
+
+
+        private boolean isPrintable(byte b) {
+            return 33 < b && b < 126;
+        }
+
+        public String next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            StringBuilder sb = new StringBuilder();
+            byte b = read();
+            while (isPrintable(b)) {
+                sb.appendCodePoint(b);
+                b = read();
+            }
+            return sb.toString();
+        }
+
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            long result = 0;
+            boolean minus = false;
+            byte b;
+
+            b = read();
+            if (b == '-') {
+                minus = true;
+                b = read();
+            }
+
+            while (isPrintable(b)) {
+                if (b < '0' || b > '9') {
+                    throw new NumberFormatException();
+                }
+                result *= 10;
+                result += (b - '0');
+                b = read();
+            }
+
+            return minus ? -result : result;
+        }
+    }
+
+//-------------------------------------------------------------------
+}
