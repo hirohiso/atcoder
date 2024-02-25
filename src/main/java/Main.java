646d576320d068af414ed2ae5616dbdef9d2944a
hiro <irie.h126@gmail.com>
2024-02-25 10:42:17 +0900

    ちょっと修正

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.Format;
 import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SuppressWarnings("unchecked")
 public class Main {
@@ -14,47 +21,104 @@ public class Main {
 
     private static void solve(PrintWriter pw, FastScanner fs) {
         //==================
+        var N = fs.ni();
+        var An = fs.nia(N);
 
-        var K = fs.nextInt();
+        var primeTable = prime(200_010);
 
-        var bit = 0;
-
-        var set = new TreeSet<Long>();
-        while (bit < 1 << 10) {
-            var t = cal(bit);
-            if (t != 0) {
-                set.add(t);
+        var ans = 0l;
+        var zeros = 0l;
+        var map2 = new HashMap<Long, Integer>();
+        for (int i = 0; i < An.length; i++) {
+            if (An[i] == 0) {
+                zeros++;
+                continue;
             }
-            bit++;
-        }
-
-        for (Long i : set) {
-            K--;
-            if (K == 0) {
-                pw.println(i);
-                return;
+            Map<Long, Integer> map = factorization(An[i], primeTable);
+            var cnt = 1l;
+            for (var e : map.entrySet()) {
+                var key = e.getKey();
+                var value = e.getValue();
+                if (value % 2 == 1) {
+                    cnt *= key;
+                }
             }
+            map2.merge(cnt, 1, Integer::sum);
         }
-        //==================
+        for (var e : map2.entrySet()) {
+            var k = e.getKey();
+            var v = (long)e.getValue();
+            ans += (v * (v - 1) / 2);
+        }
+        var nl = (long)N;
+        ans += (nl * (nl - 1) / 2) - ((nl - zeros) * (nl - zeros - 1) / 2);
+        pw.println(ans);
     }
 
-    public static long cal(int bit) {
-        var ret = 0L;
-        var dig = 0L;
-        var exp = 1L;
-        while (bit > 0) {
-            if ((bit & 1) == 1) {
-                ret += dig * exp;
-                exp *= 10;
+
+    //Nまでの素数を判定する
+    public static int[] prime(int n) {
+        int[] result = new int[n + 1];
+        Arrays.fill(result, 0);
+        for (int i = 0; i < result.length; i++) {
+            result[i] = i;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if(result[i] == i){
+                for (int j = i * i; j <= n; j+=i) {
+                    if(result[j] == j){
+                        result[j] = i;
+                    }
+                }
             }
-            bit >>= 1;
-            dig++;
+        }
+        return result;
+    }
+
+    //素因数分解
+    public static Map<Long, Integer> factorization(int n, int[] spf) {
+        Map<Long, Integer> map = new HashMap<>();
+        while (n != 1){
+            map.merge((long)spf[n], 1 , Integer::sum);
+            n /= spf[n];
+        }
+        return map;
+    }
+
+
+    //--------------
+    record TPair<S, T>(S a, T b) {
+    }
+
+    record TTri<S, T, U>(S a, T b, U c) {
+    }
+
+    record Pair(long a, long b) {
+    }
+
+    record Triple(long a, long b, long c) {
+    }
+
+    public static int[] concat(int[] a, int[] b) {
+        var ret = new int[a.length + b.length];
+        for (int i = 0; i < a.length; i++) {
+            ret[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            ret[i + a.length] = b[i];
         }
         return ret;
     }
 
-    //--------------
-    record Pair(int a, int b) {
+    public static long[] concat(long[] a, long[] b) {
+        var ret = new long[a.length + b.length];
+        for (int i = 0; i < a.length; i++) {
+            ret[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            ret[i + a.length] = b[i];
+        }
+        return ret;
     }
 
 
@@ -71,6 +135,99 @@ public class Main {
 
 //-------------------------------------------------------------------
 
+    /**
+     * 各インデックスが配列の長さ以内に収まっているか境界チェックを行う
+     * <p>
+     * 多次元配列のチェックをいちいち書くのがしんどい時に。
+     * arrayBound(new int[]{1,2,3} , new int[]{3,4,3})
+     *
+     * @param target 配列に設定したいインデックス
+     * @param len    　配列の長さ
+     * @return 配列の長さ内に収まってる時 true
+     */
+    private static boolean inarr(int[] target, int[] len) {
+        var b = true;
+        if (target.length != len.length) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < target.length; i++) {
+            b &= (0 <= target[i] && target[i] < len[i]);
+        }
+        return b;
+    }
+
+    private static int[] arr(int... a) {
+        return Arrays.copyOf(a, a.length);
+    }
+
+    private static long[] arr(long... a) {
+        return Arrays.copyOf(a, a.length);
+    }
+
+    //半時計90度回転
+    private static int[][] rot(int[][] grid) {
+        var h = grid.length;
+        var w = grid[0].length;
+
+        var result = new int[w][h];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                result[w - 1 - j][i] = grid[i][j];
+            }
+        }
+        return result;
+    }
+
+
+    //http://fantom1x.blog130.fc2.com/blog-entry-194.html
+
+    /**
+     * <h1>指定した値以上の先頭のインデクスを返す</h1>
+     * <p>配列要素が０のときは、０が返る。</p>
+     *
+     * @param arr   ： 探索対象配列(単調増加であること)
+     * @param value ： 探索する値
+     * @return<b>int</b> ： 探索した値以上で、先頭になるインデクス
+     */
+    public static final int lowerBound(final long[] arr, final long value) {
+        int low = 0;
+        int high = arr.length;
+        int mid;
+        while (low < high) {
+            mid = ((high - low) >>> 1) + low;    //(low + high) / 2 (オーバーフロー対策)
+            if (arr[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    /**
+     * <h1>指定した値より大きい先頭のインデクスを返す</h1>
+     * <p>配列要素が０のときは、０が返る。</p>
+     *
+     * @param arr   ： 探索対象配列(単調増加であること)
+     * @param value ： 探索する値
+     * @return<b>int</b> ： 探索した値より上で、先頭になるインデクス
+     */
+    public static final int upperBound(final long[] arr, final long value) {
+        int low = 0;
+        int high = arr.length;
+        int mid;
+        while (low < high) {
+            mid = ((high - low) >>> 1) + low;    //(low + high) / 2 (オーバーフロー対策)
+            if (arr[mid] <= value) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    //----------------
     public static class FastScanner {
         InputStream in;
         byte[] buffer = new byte[1 << 10];
@@ -139,39 +296,67 @@ public class Main {
             return sb.toString();
         }
 
-        public String next() {
+        public String n() {
             return innerNext(b -> (33 <= b && b <= 126));
         }
 
-        public int nextInt() {
-            return (int) nextLong();
+        public int ni() {
+            return (int) nl();
         }
 
-        public int[] nextIntArray(int n) {
+        public char[][] ncaa(int n, int m) {
+            var grid = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                grid[i] = n().toCharArray();
+            }
+            return grid;
+        }
+
+        public int[] nia(int n) {
             int[] result = new int[n];
             for (int i = 0; i < n; i++) {
-                result[i] = nextInt();
+                result[i] = ni();
             }
             return result;
         }
 
-        public String[] nextArray(int n) {
+        public int[][] niaa(int h, int w) {
+            int[][] result = new int[h][w];
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    result[i][j] = ni();
+                }
+            }
+            return result;
+        }
+
+        public long[][] nlaa(int h, int w) {
+            long[][] result = new long[h][w];
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    result[i][j] = nl();
+                }
+            }
+            return result;
+        }
+
+        public String[] na(int n) {
             String[] result = new String[n];
             for (int i = 0; i < n; i++) {
-                result[i] = next();
+                result[i] = n();
             }
             return result;
         }
 
-        public long[] nextLongArray(int n) {
+        public long[] nla(int n) {
             long[] result = new long[n];
             for (int i = 0; i < n; i++) {
-                result[i] = nextLong();
+                result[i] = nl();
             }
             return result;
         }
 
-        public long nextLong() {
+        public long nl() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
